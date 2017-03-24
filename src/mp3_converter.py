3f1315a8faecb4d9557538ea63e519cc2b2f2c4e
Ubuntu <ubuntu@ip-172-31-2-98.us-west-2.compute.internal>
2017-03-24 23:03:02 +0000

    Minor cleanup

--- src/mp3_converter.py
+++ src/mp3_converter.py
@@ -1,14 +1,7 @@
-# from pylab import plot, show, title, xlabel, ylabel, subplot, savefig
 from scipy import fft, arange, ifft
 from numpy import sin, linspace, pi
-# from scipy.io.wavfile import read,write
 from pydub import AudioSegment
-# import pydub
 import glob
-
-# Getting beatport previews: http://geo-samples.beatport.com/lofi/9040275.LOFI.mp3
-
-
 
 def mp3_to_wav(path):
     songdirs = glob.glob(path+'*.mp3')
@@ -18,9 +11,4 @@ def mp3_to_wav(path):
 
 
 if __name__ == '__main__':
-    # songdirs = glob.glob('../data/songs/aero-manyelo/'+'*.mp3')
-    # for song in songdirs:
-    #     sound = AudioSegment.from_mp3(song)
-    #     sound.export('../data/songs/aero-manyelo/wav/'+song.split('/')[-1][:-4]+'.wav', format="wav")
-
-    mp3_to_wav('../data/songs/aero-manyelo/')
+    # mp3_to_wav('../data/songs/aero-manyelo/')
