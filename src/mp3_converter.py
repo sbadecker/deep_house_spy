cf1c8cb3265c30c4214c00c6a821f28a52a317ad
Ubuntu <ubuntu@ip-172-31-2-98.us-west-2.compute.internal>
2017-03-24 18:17:19 +0000

    Fixed mp3_to_wav

--- src/mp3_converter.py
+++ src/mp3_converter.py
@@ -1,16 +1,26 @@
-from pylab import plot, show, title, xlabel, ylabel, subplot, savefig
+# from pylab import plot, show, title, xlabel, ylabel, subplot, savefig
 from scipy import fft, arange, ifft
 from numpy import sin, linspace, pi
-from scipy.io.wavfile import read,write
+# from scipy.io.wavfile import read,write
 from pydub import AudioSegment
-import pydub
+# import pydub
+import glob
 
 # Getting beatport previews: http://geo-samples.beatport.com/lofi/9040275.LOFI.mp3
 
-sound = AudioSegment.from_mp3("/path/to/file.mp3")
-sound.export("/output/path", format="wav")
 
-def mp3_to_wav(file_in):
-    sound = pydub.AudioSegment.from_mp3(filename)
-    file_out = file_in[:-4]+'.wav'
-    sound.export(file_out)
+
+def mp3_to_wav(path):
+    songdirs = glob.glob(path+'*.mp3')
+    for song in songdirs:
+        sound = AudioSegment.from_mp3(song)
+        sound.export(path+'wav/'+song.split('/')[-1][:-4]+'.wav', format="wav")
+
+
+if __name__ == '__main__':
+    # songdirs = glob.glob('../data/songs/aero-manyelo/'+'*.mp3')
+    # for song in songdirs:
+    #     sound = AudioSegment.from_mp3(song)
+    #     sound.export('../data/songs/aero-manyelo/wav/'+song.split('/')[-1][:-4]+'.wav', format="wav")
+
+    mp3_to_wav('../data/songs/aero-manyelo/')
