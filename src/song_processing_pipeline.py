dc8427a2a7c53934e6eda490a709d64536edb7f6
Ubuntu <ubuntu@ip-172-31-2-98.us-west-2.compute.internal>
2017-03-30 18:29:22 +0000

    Added song_combiner

--- src/song_processing_pipeline.py
+++ src/song_processing_pipeline.py
@@ -4,14 +4,14 @@ import os
 import librosa
 import multiprocessing
 from functools import partial
-from helper_tools import pickle_exporter
+from helper_tools import pickle_exporter, move_done
 from full_model import snippet_feature_extractor
 
 #########################################
 ########### Audio extraction ############
 #########################################
 
-def parallel_audio_extractor(path, format='mp3', duration=None, offset=0.0, song_limit=None, sample_rate=22050, pool_size=6, batch_size=24):
+def parallel_audio_extractor(path, format='mp3', duration=None, offset=0.0, song_limit=None, sample_rate=22050, pool_size=7, batch_size=24):
     '''
     Takes in the path to audio files (mp3) and loads them as floating time series.
     It calls the pickle_exporter which stores the audio and meta data as individual
@@ -39,13 +39,13 @@ def parallel_audio_extractor(path, format='mp3', duration=None, offset=0.0, song
 ########### Feature extraction ##########
 #########################################
 
-def parallel_feature_extractor(path, n_mfcc=20, second_snippets=1, pool_size=8, sample_rate=22050):
+def parallel_feature_extractor(path, n_mfcc=20, second_snippets=1, pool_size=7, sample_rate=22050, i=0):
     '''
     Takes in a path and loads in all .npy files included in this directory. It
     the splits the files in snippets and extracts the MFCCs from saves them in
     a new directory as npys.
     '''
-    raw_songdirs = glob.glob(path+'*.npy')
+    raw_songdirs = glob.glob(path+'*.npy')[i:]
     pool = multiprocessing.Pool(processes=pool_size)
     X = pool.map(partial(parallel_feature_child, path=path, frames=sample_rate*second_snippets, n_mfcc=n_mfcc), raw_songdirs)
     pool.close()
@@ -65,6 +65,37 @@ def parallel_feature_child(song, path, frames, n_mfcc):
         X_song.append(snippet_features_raw)
     np.save(path+'features_extracted/'+song.split('/')[-1][:-4], X_song, allow_pickle=True)
 
+
+#########################################
+########### Feature extraction ##########
+#########################################
+
+def song_combiner(path):
+    '''
+    Takes in a path with npy files that from the parallel_feature_extractor and
+    combines those into X  (n_songs x n_snippets x n_mfccs x time_frames) and
+    y (n_songs x n_snippets x 1).
+    '''
+    X = []
+    y = []
+    song_dirs = glob.glob(path+'*.npy')[:10]
+    for song in song_dirs:
+        X_song = np.load(song)
+        artist = [int(song.split('/')[-1].split('_')[0])]
+        y_song = artist * X_song.shape[0]
+        X.append(X_song)
+        y.append(y_song)
+    return np.array(X), np.array(y)
+
+
 if __name__ == '__main__':
-    parallel_audio_extractor('../data/100_artists/mp3s/')
-    parallel_feature_extractor('../data/100_artists/mp3s/output/', pool_size=8)
+    parallel_audio_extractor('../data/100_artists/mp3s2/')
+    # parallel_feature_extractor('../data/100_artists/mp3s2/output/')
+    i = 0
+    while len(glob.glob('../data/100_artists/mp3s2/output/*.*'))>i:
+        try:
+            parallel_feature_extractor('../data/100_artists/mp3s2/output/', pool_size=7, i=i)
+        except:
+            move_done('/home/ubuntu/deep_house_spy/deep_house_spy/data/100_artists/mp3s2/output/', '/home/ubuntu/deep_house_spy/deep_house_spy/data/100_artists/mp3s2/output/features_extracted/', 'npy')
+            i += 1
+            print i
