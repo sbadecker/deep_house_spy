ab380c3a5bc7eda96f3e6179bd99f61dd9a2e56d
Ubuntu <ubuntu@ip-172-31-2-98.us-west-2.compute.internal>
2017-03-30 19:49:25 +0000

    Minor changes

--- src/song_processing_pipeline.py
+++ src/song_processing_pipeline.py
@@ -55,6 +55,7 @@ def parallel_feature_child(song, path, frames, n_mfcc):
     if not os.path.exists(path+'features_extracted/'):
         os.makedirs(path+'features_extracted/')
     X_song = []
+    print song
     raw_audio_song = np.load(song)
     total_splits = raw_audio_song.shape[0]/frames
     splits = [n*frames for n in range(1, 120)]
@@ -74,7 +75,7 @@ def song_combiner(path):
     '''
     Takes in a path with npy files that from the parallel_feature_extractor and
     combines those into X  (n_songs x n_snippets x n_mfccs x time_frames) and
-    y (n_songs x n_snippets x 1).
+    y (n_songs x n_snippets).
     '''
     X = []
     y = []
@@ -92,13 +93,13 @@ def song_combiner(path):
 
 
 if __name__ == '__main__':
-    parallel_audio_extractor('../data/100_artists/mp3s2/')
+    # parallel_audio_extractor('../data/100_artists/mp3s2/')
     # parallel_feature_extractor('../data/100_artists/mp3s2/output/')
-    i = 0
-    while len(glob.glob('../data/100_artists/mp3s2/output/*.*'))>i:
-        try:
-            parallel_feature_extractor('../data/100_artists/mp3s2/output/', pool_size=7, i=i)
-        except:
-            move_done('/home/ubuntu/deep_house_spy/deep_house_spy/data/100_artists/mp3s2/output/', '/home/ubuntu/deep_house_spy/deep_house_spy/data/100_artists/mp3s2/output/features_extracted/', 'npy')
-            i += 1
-            print i
+    # i = 8
+    # while len(glob.glob('../data/100_artists/mp3s2/output/*.*'))>i:
+    #     try:
+    #         parallel_feature_extractor('../data/100_artists/mp3s2/output/', pool_size=7, i=i)
+    #     except:
+    move_done('/home/ubuntu/deep_house_spy/deep_house_spy/data/100_artists/mp3s2/output/', '/home/ubuntu/deep_house_spy/deep_house_spy/data/100_artists/mp3s2/output/features_extracted/', 'npy')
+            # i += 1
+            # print i
